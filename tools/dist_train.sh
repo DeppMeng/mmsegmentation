b5289d92bc9d4f9da7217aa26bd32903ea1addd3
DeppMeng <mdpustc@gmail.com>
2020-08-18 16:39:04 +0800

    added scripts

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+PYTHON="/opt/conda/bin/python"
+
+$PYTHON -m pip install mmcv
+$PYTHON -m pip install -e .
+
 CONFIG=$1
 GPUS=$2
 PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
+${PYTHON} -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+    $(dirname "$0")/train.py $CONFIG --launcher pytorch --seed 0
