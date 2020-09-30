b06bf49c737bbbc5e887ed3ae2e3ba5b238e95af
Depu Meng (FA Talent) <v-depme@microsoft.com>
2020-09-30 17:13:03 +0800

    added scripts

---
+++ configs/deeplabv3/deeplabv3_r50-d8_512x1024_40k_cityscapes_std_exemplar_v2_1m_t01_pyres.py
@@ -0,0 +1,4 @@
+_base_ = [
+    '../_base_/models/deeplabv3_r50-d8-std-exemplar_v2_1m_t01_pyres.py', '../_base_/datasets/cityscapes.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
+]
